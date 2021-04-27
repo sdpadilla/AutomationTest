ddc11c34123fad295554534da39dcde8be9798c0
Silvana Padilla <sd.padilla@outlook.com>
2021-04-27 07:39:01 -0400

    verify mail test

--- VerifyMail/UnitTest1.cs
+++ VerifyMail/UnitTest1.cs
@@ -1,18 +1,57 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
 
 namespace VerifyMail
 {
     public class Tests
     {
+        String test_url = "https://reqres.in/";
+        IWebDriver driver;
+
         [SetUp]
-        public void Setup()
+        public void start_Browser()
         {
+            // Local Selenium WebDriver
+            var op = new FirefoxOptions
+            {
+                AcceptInsecureCertificates = true
+            };
+            driver = new FirefoxDriver(op);
+            driver.Manage().Window.Maximize();
         }
 
         [Test]
-        public void Test1()
+        public void test_search()
         {
-            Assert.Pass();
+            driver.Url = test_url;
+
+
+
+            System.Threading.Thread.Sleep(500);
+
+            IWebElement searchPage2 = driver.FindElement(By.XPath("/html/body/div[2]/div/div/section[1]/div[2]/div[1]/p/strong/a/span"));
+
+
+
+
+            searchPage2.Click();
+
+            IWebElement searchText = driver.FindElement(By.XPath("/html/body/div/div/div/div[1]/div/div/div[1]/input"));
+
+            searchText.SendKeys("michael.lawson@reqres.in");
+
+
+            System.Threading.Thread.Sleep(6000);
+
+            Console.WriteLine("Test Passed");
+        }
+
+        [TearDown]
+        public void close_Browser()
+        {
+            driver.Quit();
         }
     }
 }
